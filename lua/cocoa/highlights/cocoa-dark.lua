fe7927f907d9161de804912e5975c539e62865eb
Jordan Houlihan <jphouli1@gmail.com>
2026-06-21 15:58:43 -0400

    Cleaned up the way highlights are loaded in

--- lua/cocoa/highlights/cocoa-dark.lua
+++ lua/cocoa/highlights/cocoa-dark.lua
@@ -7,131 +7,151 @@ function M.setup()
     local palette = require("cocoa.palettes.cocoa-dark")
     local highlight = vim.api.nvim_set_hl
 
-    highlight(0, "Normal", {
-        fg = palette.light_red,
-        bg = palette.bg
-    })
-    highlight(0, "Statement", {
-        fg = palette.light_yellow,
-        bg = palette.bg
-    })
-    highlight(0, "Delimiter", {
-        fg = palette.light_yellow_green,
-        bg = palette.bg
-    })
-    highlight(0, "PreProc", {
-        fg = palette.light_green,
-        bg = palette.bg
-    })
-    highlight(0, "Special", {
-        fg = palette.light_green,
-        bg = palette.bg
-    })
-    highlight(0, "Operator", {
-        fg = palette.light_yellow_green,
-        bg = palette.bg
-    })
-    highlight(0, "Constant", {
-        fg = palette.red,
-        bg = palette.bg
-    })
-    highlight(0, "Function", {
-        fg = palette.orange,
-        bg = palette.bg
-    })
-    highlight(0, "String", {
-        fg = palette.yellow,
-        bg = palette.bg
-    })
-    highlight(0, "EndOfBuffer", {
-        fg = palette.light_red,
-        bg = palette.bg
-    })
-    highlight(0, "CursorLine", {
-        fg = palette.bg,
-        bg = palette.light_orange
-    })
-    highlight(0, "LineNr", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "CursorLineNr", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "Visual", {
-        fg = palette.bg,
-        bg = palette.light_orange
-    })
-    highlight(0, "Search", {
-        fg = palette.bg,
-        bg = palette.light_orange
-    })
-    highlight(0, "Pmenu", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "PmenuSel", {
-        fg = palette.bg,
-        bg = palette.light_orange
-    })
-    highlight(0, "Comment", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "Identifier", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "Type", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "Keyword", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "Structure", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@variable", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@function.call", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@method.call", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@parameter", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@field", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@property", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@constructor", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@keyword.return", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
-    highlight(0, "@tag", {
-        fg = palette.light_orange,
-        bg = palette.bg
-    })
+    local highlightSettings = vim.tbl_extend("force", getUIHighlightSettings(palette),
+        getTreeSitterHighlightSettings(palette), getBaseHighlightSettings(palette))
 
+    for group, colorSettings in pairs(highlightSettings) do
+        highlight(0, group, colorSettings)
+    end
+end
+
+function getUIHighlightSettings(palette)
+    return {
+        CursorLine = {
+            fg = palette.bg,
+            bg = palette.light_red
+        },
+        LineNr = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        CursorLineNr = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Visual = {
+            fg = palette.bg,
+            bg = palette.light_red
+        },
+        Search = {
+            fg = palette.bg,
+            bg = palette.light_red
+        },
+        Pmenu = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        PmenuSel = {
+            fg = palette.bg,
+            bg = palette.light_red
+        }
+    }
+end
+
+function getTreeSitterHighlightSettings(palette)
+    return {
+        ["@field"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@variable"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@function.call"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@method.call"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@parameter"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@property"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@constructor"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@keyword.return"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        ["@tag"] = {
+            fg = palette.light_red,
+            bg = palette.bg
+        }
+    }
+end
+
+function getBaseHighlightSettings(palette)
+    return {
+        Normal = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        EndOfBuffer = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Statement = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Delimiter = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Operator = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Constant = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        String = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Function = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        PreProc = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Special = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Comment = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Identifier = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Type = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Keyword = {
+            fg = palette.light_red,
+            bg = palette.bg
+        },
+        Structure = {
+            fg = palette.light_red,
+            bg = palette.bg
+        }
+    }
 end
 
 return M
