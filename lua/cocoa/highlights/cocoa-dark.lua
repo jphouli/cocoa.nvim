0612420800848534d4b9e4580c64cde0c800457e
Jordan Houlihan <jphouli1@gmail.com>
2026-06-27 17:24:09 -0400

    too green earlier

--- lua/cocoa/highlights/cocoa-dark.lua
+++ lua/cocoa/highlights/cocoa-dark.lua
@@ -24,7 +24,7 @@ function getUIHighlightSettings(palette)
             fg = palette["slate-500"]
         },
         CursorLineNr = {
-            fg = palette["mango-300"],
+            fg = palette["mango-500"],
             bold = true
         },
         Visual = {
@@ -32,7 +32,7 @@ function getUIHighlightSettings(palette)
         },
         Search = {
             fg = palette["slate-900"],
-            bg = palette["deep_gold-300"]
+            bg = palette["mango-300"]
         },
         Pmenu = {
             fg = palette["slate-100"],
@@ -40,7 +40,7 @@ function getUIHighlightSettings(palette)
         },
         PmenuSel = {
             fg = palette["slate-900"],
-            bg = palette["mango-300"],
+            bg = palette["mango-500"],
             bold = true
         }
     }
@@ -49,28 +49,29 @@ end
 function getTreeSitterHighlightSettings(palette)
     return {
         ["@field"] = {
-            fg = palette["mango-100"]
+            fg = palette["mango-300"]
         },
         ["@variable"] = {
             fg = palette["mango-100"]
         },
         ["@variable.member"] = {
-            fg = palette["mango-100"]
+            fg = palette["mango-300"]
         },
         ["@parameter"] = {
             fg = palette["slate-300"]
         },
         ["@property"] = {
-            fg = palette["mango-100"]
+            fg = palette["mango-300"]
         },
         ["@function.call"] = {
-            fg = palette["deep_gold-500"]
+            fg = palette["mango-500"],
+            bold = true
         },
         ["@method.call"] = {
-            fg = palette["deep_gold-500"]
+            fg = palette["mango-500"]
         },
         ["@constructor"] = {
-            fg = palette["deep_gold-300"]
+            fg = palette["deep_gold-500"]
         },
         ["@keyword.return"] = {
             fg = palette["mango-500"],
@@ -105,22 +106,22 @@ function getBaseHighlightSettings(palette)
             fg = palette["matcha-300"]
         },
         Function = {
-            fg = palette["deep_gold-500"]
+            fg = palette["mango-500"]
         },
         PreProc = {
-            fg = palette["mango-300"]
+            fg = palette["deep_gold-500"]
         },
         Special = {
-            fg = palette["mango-300"]
+            fg = palette["deep_gold-300"]
         },
         Type = {
-            fg = palette["deep_gold-300"]
+            fg = palette["deep_gold-500"]
         },
         Identifier = {
             fg = palette["mango-100"]
         },
         Directory = {
-            fg = palette["deep_gold-300"]
+            fg = palette["mango-500"]
         },
         Keyword = {
             fg = palette["mango-500"],
@@ -146,7 +147,7 @@ function getBaseHighlightSettings(palette)
             bold = true
         },
         Structure = {
-            fg = palette["mango-300"]
+            fg = palette["deep_gold-500"]
         },
         Delimiter = {
             fg = palette["slate-300"]
