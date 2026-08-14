519052f1a0f91a0d3f3d76ff017865af7ea1a26c
Jordan Houlihan <jphouli1@gmail.com>
2026-08-13 21:42:38 -0400

    fixing setup function

--- lua/cocoa/init.lua
+++ lua/cocoa/init.lua
@@ -7,7 +7,7 @@ function M.setup(opts)
     vim.g.colors_name = opts.style
     vim.o.background = opts.mode
 
-    require('lua.cocoa.color_groups.base').setup()
+    require('cocoa.color_groups.base').setup()
 end
 
 return M
