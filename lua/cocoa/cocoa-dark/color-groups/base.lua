9d9d4b0510ddb01d9c8c2e059881df5bd1a40c03
Jordan Houlihan <jphouli1@gmail.com>
2026-08-17 15:07:43 -0400

    change background

--- lua/cocoa/cocoa-dark/color-groups/base.lua
+++ lua/cocoa/cocoa-dark/color-groups/base.lua
@@ -4,23 +4,23 @@ function M.setup(palette)
     return {
         Normal = {
             fg = palette.dusty_brown,
-            bg = palette.olive_charcoal
+            bg = palette.olive_gray
         },
         NonText = {
             fg = palette.dusty_brown,
-            bg = palette.olive_charcoal
+            bg = palette.olive_gray
         },
         PreProc = {
             fg = palette.olive_green,
-            bg = palette.olive_charcoal
+            bg = palette.olive_gray
         },
         String = {
             fg = palette.spring_green,
-            bg = palette.olive_charcoal
+            bg = palette.olive_gray
         },
         Type = {
             fg = palette.coral,
-            bg = palette.olive_charcoal
+            bg = palette.olive_gray
         }
     }
 end
