159a6dbec3876c0c953dd8716bd5e56c9e84db90
Jordan Houlihan <jphouli1@gmail.com>
2026-08-18 10:44:19 -0400

    adding line number highlight groups

--- lua/cocoa/cocoa-dark/color-groups/base.lua
+++ lua/cocoa/cocoa-dark/color-groups/base.lua
@@ -6,7 +6,15 @@ function M.setup(palette)
             fg = palette.beige,
             bg = palette.brown
         },
+        CursorLineNr = {
+            fg = palette.beige,
+            bg = palette.brown
+        },
         EndOfBuffer = {
+            fg = palette.muted_taupe,
+            bg = palette.brown
+        },
+        LineNr = {
             fg = palette.muted_taupe,
             bg = palette.brown
         },
